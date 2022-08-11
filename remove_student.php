eaaeb366dd6483c7d54443b0e0f68011b6c2e462
Eric9546 <ericmok85@gmail.com>
2022-08-11 16:53:21 +0800

    Update remove_student.php

--- remove_student.php
+++ remove_student.php
@@ -1,44 +1,46 @@
 <?php
 
-// Declaring function for alert message //
-function alert ($msg)
-{
+    session_start ();
 
-    echo "<script type='text/javascript'>alert('$msg'); window.location='view_user.php';</script>";
+    // Declaring function for alert message //
+    function alert ($msg)
+    {
 
-}
+        echo "<script type='text/javascript'>alert('$msg'); window.location='view_user.php';</script>";
 
-// Establishing connection to database //
-include ('dbcon.php');
+    }
 
-// Gathering the input from user //
-$record_to_remove = $_POST ['record_to_remove'];
-$stuId = $_POST ['stuId'];
-$program = $_POST ['program'];
-$session = $_POST ['session'];
+    // Establishing connection to database //
+    include ('dbcon.php');
 
-// Query to delele the record from the database table //
-$path = 'Registration/' . $record_to_remove;
-$reference = $database->getReference($path)->remove();
+    // Gathering the input from user //
+    $record_to_remove = $_POST ['record_to_remove'];
+    $stuId = $_POST ['stuId'];
+    $program = $_POST ['program'];
+    $session = $_POST ['session'];
 
-// Logic to check if the record was deleted successfully //
-if ($reference)
-{
+    // Query to delele the record from the database table //
+    $path = 'Registration/' . $record_to_remove;
+    $reference = $database->getReference($path)->remove();
 
-    $_SESSION ['log_id'] = $_SESSION ['id'];
-    $_SESSION ['log_stuId'] = $record_to_edit;
-    $_SESSION ['log_program'] = $program;
-    $_SESSION ['log_session'] = $session;
+    // Logic to check if the record was deleted successfully //
+    if ($reference)
+    {
 
-    header ("Location: log_remove_student.php");
+        $_SESSION ['log_id'] = $_SESSION ['id'];
+        $_SESSION ['log_stuId'] = $record_to_edit;
+        $_SESSION ['log_program'] = $program;
+        $_SESSION ['log_session'] = $session;
 
-}
+        header ("Location: log_remove_student.php");
 
-else
-{
+    }
 
-    alert ("Student Was Unable To Be Removed!");
+    else
+    {
 
-}
+        alert ("Student Was Unable To Be Removed!");
+
+    }
 
 ?>
